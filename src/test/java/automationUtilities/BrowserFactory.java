93d8b566f28c4a4be6b29dd279dc2260005659da
smrutisdet <smmohapatra011@gmail.com>
2024-02-24 22:29:33 +0530

    Implement BrowserFactory

---
+++ src/test/java/automationUtilities/BrowserFactory.java
@@ -0,0 +1,46 @@
+package automationUtilities;
+
+import org.openqa.selenium.WebDriver;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ThreadGuard;
+
+import java.util.Properties;
+
+public class BrowserFactory {
+    public static final ThreadLocal<WebDriver>driver=new ThreadLocal<>();
+    private Logger log;
+    private Properties prop;
+    private String browserName;
+    private String applicationURL;
+    public WebDriver browserSetup(){
+        try{
+            log=LogManager.getLogger(this.getClass().getName());
+            prop=new Properties();
+            prop.load(BrowserFactory.class.getClassLoader().getResourceAsStream("configuration.properties"));
+            browserName=System.getProperty("browser");
+            log.info("Browser is"+browserName);
+            if(browserName.equalsIgnoreCase("chrome")){
+                driver.set(ThreadGuard.protect(new ChromeDriver()));
+            }
+            else if(browserName.equalsIgnoreCase("edge")){
+                driver.set(ThreadGuard.protect(new EdgeDriver()));
+            }
+            else if(browserName.equalsIgnoreCase("firefox")){
+                driver.set(ThreadGuard.protect(new FirefoxDriver()));
+            }
+            else{
+                log.info("unsupported browser is passed:"+browserName);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return getDriverInstance();
+    }
+    public static synchronized WebDriver getDriverInstance(){
+        return driver.get();
+    }
+}
